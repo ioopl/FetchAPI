3c704540921c2df57838246500c0e222fc485172
Umair Hasan <umair.hasan@ocado.com>
2020-03-01 20:08:09 +0000

    Added IBOutlets and connected them to Controller file

--- FetchAPI/LoginViewController.swift
+++ FetchAPI/LoginViewController.swift
@@ -3,8 +3,15 @@ import RxSwift
 
 final class LoginViewController: UIViewController {
 
+    @IBOutlet weak var txtAppID: UITextField!
+    @IBOutlet weak var txtAppKey: UITextField!
+    @IBOutlet weak var lblMessage: UILabel!
+    @IBOutlet weak var btnSend: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
     }
 
 }
