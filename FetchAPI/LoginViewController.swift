f7f18089cced26c4bc7a819841c8eafb1bc6ecbb
Umair Hasan <umair.hasan@ocado.com>
2020-03-01 20:36:37 +0000

    Added logic to enable/disbale the button based on the input from textfield

--- FetchAPI/LoginViewController.swift
+++ FetchAPI/LoginViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import RxSwift
+import RxCocoa
 
 final class LoginViewController: UIViewController {
 
@@ -7,12 +8,23 @@ final class LoginViewController: UIViewController {
     @IBOutlet weak var txtAppKey: UITextField!
     @IBOutlet weak var lblMessage: UILabel!
     @IBOutlet weak var btnSend: UIButton!
+    private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
+        txtAppKey.rx.text.orEmpty // Observebale<String>
+                .map { !$0.isEmpty } // Observeable<Bool>
+                .subscribe(onNext: { [unowned self] (isEnabled) in
+                self.btnSend.isEnabled = isEnabled
+            }).disposed(by: disposeBag)
+
+        btnSend.rx.tap
+            .withLatestFrom(txtAppKey.rx.text.orEmpty)
+            //.map { "Hello \($0)" } // Observeable<String>
+            .subscribe(onNext: { [unowned self] text in
+                self.lblMessage.text = text
+            }).disposed(by: disposeBag)
     }
-
 }
 
